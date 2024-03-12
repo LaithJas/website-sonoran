c4c0cd9adf89e10c3522d3b056b3e1fc4dc71f72
Laith Abou Saleh <laithas.007@gmail.com>
2024-03-11 22:53:00 -0700

    removed map comp for testing

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,6 @@ function App() {
                     <div>
                         <Home setSelectedPage={setSelectedPage} />
                         <Services setSelectedPage={setSelectedPage} />
-                        <MapComponent />
                     </div>
                 } />
 
